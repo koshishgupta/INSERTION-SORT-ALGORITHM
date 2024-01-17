932e29f029d3f8e4bee2532d2997b69eae568b0f
koshishgupta <guptakoshish5@gmail.com>
2024-01-17 18:05:11 +0530

    Add project files.

---
+++ INSERTION SORT ALGORITHM/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace INSERTION_SORT_ALGORITHM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER A ARRAY SIZE:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] arr=new int[n];
+            for(int i=0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0;i<n;i++)
+            {
+                for(int j = n; j >= 1; j--)
+                {
+                    if(arr[j - 1] > arr[j])
+                    {
+                        int temp = arr[j-1];
+                        arr[j-1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            for(int i=0;i<n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
